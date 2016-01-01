d1b08734b93543abc551751c4d0c1113c54ab3cd
Roman Panteleev <romaxa95.95@gmail.com>
2016-01-01 23:42:09 +0300

    запись в файл результатов вычисления

--- KSKR/UI/DataForm.Designer.cs
+++ KSKR/UI/DataForm.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SaveDataInFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -40,11 +43,28 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(408, 448);
+            this.dataGridView1.Size = new System.Drawing.Size(408, 433);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SaveDataInFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(432, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // SaveDataInFileToolStripMenuItem
+            // 
+            this.SaveDataInFileToolStripMenuItem.Name = "SaveDataInFileToolStripMenuItem";
+            this.SaveDataInFileToolStripMenuItem.Size = new System.Drawing.Size(174, 20);
+            this.SaveDataInFileToolStripMenuItem.Text = "Сохранить результат в файл";
+            this.SaveDataInFileToolStripMenuItem.Click += new System.EventHandler(this.SaveDataInFileToolStripMenuItem_Click);
             // 
             // DataForm
             // 
@@ -52,15 +72,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(432, 472);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "DataForm";
             this.Text = "DataForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem SaveDataInFileToolStripMenuItem;
     }
 }
--- KSKR/UI/DataForm.cs
+++ KSKR/UI/DataForm.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Windows.Forms;
 using Domain.Common;
+using System.IO;
 
 namespace UI
 {
@@ -66,6 +67,117 @@ namespace UI
             Height = cellHeight * rows + 75;
             dataGridView1.Width = cellwidth * column + 40;
             dataGridView1.Height = cellHeight * rows + 60;
+        }
+
+        private void SaveDataInFileToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                int oneCellWidth = GetMaxLengthString(0);
+                int otherCellWidth = GetMaxLengthString(1);
+                
+                FileStream fs = new FileStream(saveFileDialog.FileName+ ".txt", FileMode.Create);
+                StreamWriter streamWriter = new StreamWriter(fs);
+                WriteLineFromSymbols(streamWriter, "-");
+                streamWriter.WriteLine();
+
+                for (int i = 0; i < dataGridView1.Rows[0].Cells.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        streamWriter.Write("| " + DataReturnForFileReader(dataGridView1.Columns[i].HeaderText, oneCellWidth) + "  ");
+                    }
+                    else
+                    {
+                        streamWriter.Write("| " + DataReturnForFileReader(dataGridView1.Columns[i].HeaderText, otherCellWidth) + "  ");
+                    }
+                }
+
+                streamWriter.WriteLine();
+                WriteLineFromSymbols(streamWriter, "+");
+                streamWriter.WriteLine();
+
+                try
+                {
+                    for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                    {
+                        for (int i = 0; i < dataGridView1.Rows[j].Cells.Count; i++)
+                        {
+                            if (i == 0)
+                            {
+                                streamWriter.Write("| " + DataReturnForFileReader(dataGridView1.Rows[j].Cells[i].Value.ToString(), oneCellWidth) + "  ");
+                            }
+                            else
+                            {
+                                streamWriter.Write("| " + DataReturnForFileReader(dataGridView1.Rows[j].Cells[i].Value.ToString(), otherCellWidth) + "  ");
+                            }
+                        }
+
+                        streamWriter.WriteLine();
+                    }
+                    streamWriter.Close();
+                    fs.Close();
+                    MessageBox.Show("Файл успешно сохранен");
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка при сохранении файла!");
+                }
+            }
+        }
+
+        private void WriteLineFromSymbols(StreamWriter streamWriter, string symbol)
+        {
+            int maxLength = 0;
+            string line = string.Empty;
+            for (int i = 0; i < dataGridView1.Rows[0].Cells.Count; i++)
+            {
+                if (i == 0)
+                {
+                    maxLength += GetMaxLengthString(0);
+                }
+                else
+                {
+                    maxLength += GetMaxLengthString(1);
+                }
+            }
+            for (int i = 0; i < maxLength+10; i++)
+            {
+                line += symbol;
+            }
+            streamWriter.Write(line);
+        }
+
+        private int GetMaxLengthString(int cellNumber)
+        {
+            int length = 0;
+            string Value = string.Empty;
+            for (int j = 0; j < dataGridView1.Rows.Count; j++)
+            {
+                if(dataGridView1.Rows[j].Cells[cellNumber].Value.ToString().Length > Value.Length)
+                {
+                    length = dataGridView1.Rows[j].Cells[cellNumber].Value.ToString().Length;
+                }
+            }
+            return length;
+        }
+
+        private string DataReturnForFileReader(string inputString, int maxLength)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < maxLength; i++)
+            {
+                try
+                {
+                    result = result + inputString[i].ToString();
+                }
+                catch(System.IndexOutOfRangeException)
+                {
+                    result = result + " ";
+                }
+            }
+            return result;
         }
     }
 }
